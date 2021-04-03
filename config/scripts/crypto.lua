c9243105331ca86d4bd619987e5c04a6223f0357
zqbxx <3808964+zqbxx@users.noreply.github.com>
2021-04-03 09:42:18 +0800

    使用lua脚本处理打开文件协议

---
+++ config/scripts/crypto.lua
@@ -0,0 +1,22 @@
+local msg = require 'mp.msg'
+
+local function on_start()
+    local streamOpenFN = mp.get_property_native("stream-open-filename")
+    if (type(streamOpenFN) ~= "string") then do return end end
+    msg.log("info", "open file: " .. streamOpenFN)
+    local file = io.open(streamOpenFN,"rb")
+    if not file then return nil end
+    local block = file:read(7)
+    file:close()
+    if (block == 'EV00001')
+    then
+        msg.log("info", "crypto marker: EV00001")
+        mp.set_property(
+            "stream-open-filename",
+            "crypto:///" .. streamOpenFN
+        )
+        msg.log("info", "stream-open-filename: crypto:///" .. streamOpenFN)
+    end
+end
+
+mp.add_hook("on_load", 50, on_start)
