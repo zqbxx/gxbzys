93378920bcbb3a457bffbf2087cfa2f15c719dbe
zqbxx <3808964+zqbxx@users.noreply.github.com>
2021-09-24 16:19:29 +0800

    修改默认插件配置，默认为无插件

--- compile.sh
+++ compile.sh
@@ -12,3 +12,4 @@ cd $output_dir
 python -m nuitka --mingw64 --show-progress --standalone --nofollow-import-to=pygments --nofollow-import-to=PIL --nofollow-import-to=pytest --nofollow-import-to=prompt_toolkit --nofollow-import-to=PyQt5 --include-package-data=qtawesome --include-package-data=qtmodern --plugin-enable=pyside2 --windows-icon-from-ico=your_player_2.ico --windows-disable-console ./YourPlayer.py
 cp -R -u ./config $output_dir/YourPlayer.dist
 cp -R -u mpv-1.dll $output_dir/YourPlayer.dist
+cp -R -u plugin.json $output_dir/YourPlayer.dist
